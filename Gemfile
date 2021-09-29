3bd26473c785029d3ee1e6298d45ab81a609baf7
laurieroy <laurieroy.dev@gmail.com>
2021-09-29 00:18:44 +0000

    Add credentials, google oauth2 login

--- Gemfile
+++ Gemfile
@@ -53,3 +53,5 @@ group :test do
 end
 
 gem 'devise'
+gem 'omniauth-google-oauth2'
+gem 'omniauth-rails_csrf_protection', '~> 1.0'
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,21 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
-  #  :timeoutable and :omniauthable
+  #  :timeoutable 
   devise :database_authenticatable, :registerable, :confirmable,
-         :lockable, :recoverable, :rememberable, :trackable, :validatable
+         :lockable, :recoverable, :rememberable, :trackable, :validatable,
+         :omniauthable, omniauth_providers: [:google_oauth2]
+         
+  def self.from_omniauth(access_token)
+    data = access_token.info
+    user = User.where(email: data['email']).first
+
+    unless user
+      user = User.create(
+        email: data['email'],
+        password: Devise.friendly_token[0,20]
+      )
+    end
+    user.confirmed_at = Time.now
+    user
+  end
 end

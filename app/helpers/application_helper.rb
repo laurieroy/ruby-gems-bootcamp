ab8bfef320d06efc5f00dc65eb11b9fe5bbca84e
laurieroy <laurieroy.dev@gmail.com>
2021-10-09 22:21:15 +0000

    Add boolen_label badge to app_helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def boolean_label(value)
+    case value
+      when true
+        content_tag(:span, value, class: "badge bg-success") #"Yes"
+      when false
+        content_tag(:span, value, class: "badge bg-danger")#"No"
+      end
+  
+  end
 end
